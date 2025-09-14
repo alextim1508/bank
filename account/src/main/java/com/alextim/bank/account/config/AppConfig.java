04196446eae7adf607a6526a4aaeefc0b84f6a43
Aleksey Timofeev <atimofeev1508@gmail.com>
2025-09-14 23:20:46 +0300

    delete gateway and config-server modules

--- account/src/main/java/com/alextim/bank/account/config/AppConfig.java
+++ account/src/main/java/com/alextim/bank/account/config/AppConfig.java
@@ -1,12 +1,10 @@
 package com.alextim.bank.account.config;
 
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableDiscoveryClient
 @EnableCaching
 @EnableFeignClients(basePackages = "com.alextim.bank.common.client")
 public class AppConfig {
--- notification/src/main/java/com/alextim/bank/notification/config/ConfigApp.java
+++ notification/src/main/java/com/alextim/bank/notification/config/ConfigApp.java
@@ -1,13 +1,11 @@
 package com.alextim.bank.notification.config;
 
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableDiscoveryClient
 @EnableCaching
 @EnableScheduling
 @EnableFeignClients(basePackages = "com.alextim.bank.common.client")
