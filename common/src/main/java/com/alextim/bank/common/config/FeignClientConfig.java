2b4b2a14e9b0605df4c0b4a6788013113a323626
Aleksey Timofeev <atimofeev1508@gmail.com>
2025-09-14 23:20:46 +0300

    delete gateway and config-server modules

--- common/src/main/java/com/alextim/bank/common/config/FeignClientConfig.java
+++ common/src/main/java/com/alextim/bank/common/config/FeignClientConfig.java
@@ -2,10 +2,17 @@ package com.alextim.bank.common.config;
 
 import com.alextim.bank.common.client.OAuth2TokenClient;
 import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Enumeration;
 
 @Configuration
 public class FeignClientConfig {
@@ -19,8 +26,32 @@ public class FeignClientConfig {
     @Bean
     public RequestInterceptor requestInterceptor(OAuth2TokenClient tokenClient) {
         return template -> {
-            String token = tokenClient.getBearerToken(clientRegistrationId, principal);
-            template.header(HttpHeaders.AUTHORIZATION, token);
+            addAuthorizationHeader(tokenClient, template);
+
+            copyXHeaders(template);
         };
+    }
+
+    private void addAuthorizationHeader(OAuth2TokenClient tokenClient, RequestTemplate template) {
+        String token = tokenClient.getBearerToken(clientRegistrationId, principal);
+        template.header(HttpHeaders.AUTHORIZATION, token);
+    }
+
+    private void copyXHeaders(RequestTemplate template) {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+
+        if (requestAttributes instanceof ServletRequestAttributes servletRequestAttributes) {
+            HttpServletRequest request = servletRequestAttributes.getRequest();
+            Enumeration<String> headerNames = request.getHeaderNames();
+
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+
+                if (headerName.toLowerCase().startsWith("x-")) {
+                    String headerValue = request.getHeader(headerName);
+                    template.header(headerName, headerValue);
+                }
+            }
+        }
     }
 }
